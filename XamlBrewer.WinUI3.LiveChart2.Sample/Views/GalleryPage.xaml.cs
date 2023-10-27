3a2ac2326f49138b55f4935db8f4f1bd4c1744b8
Diederik Krols <diederik_krols@hotmail.com>
2023-10-27 17:01:35 +0200

    Add project files.

---
+++ XamlBrewer.WinUI3.LiveChart2.Sample/Views/GalleryPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace XamlBrewer.WinUI3.LiveCharts2.Sample.Views
+{
+    public sealed partial class GalleryPage : Page
+    {
+        public GalleryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ XamlBrewer.WinUI3.LiveChart2.Sample/Views/HomePage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.UI.Xaml.Controls;
+namespace XamlBrewer.WinUI3.LiveCharts2.Sample.Views
+{
+    public sealed partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
