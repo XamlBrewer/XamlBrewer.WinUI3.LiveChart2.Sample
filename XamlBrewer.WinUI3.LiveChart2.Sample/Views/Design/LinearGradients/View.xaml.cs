9fa86372e81b9feb4fceda01d463f028fb0eb7ba
Diederik Krols <diederik_krols@hotmail.com>
2023-10-28 22:23:44 +0200

    Gallery.

---
+++ XamlBrewer.WinUI3.LiveChart2.Sample/Views/Design/LinearGradients/View.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace WinUISample.Design.LinearGradients
+{
+    public sealed partial class View : UserControl
+    {
+        public View()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
