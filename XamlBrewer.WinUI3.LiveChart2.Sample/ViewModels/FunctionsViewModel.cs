07c10595a81a7e823232db7a82a44cc24a71fa74
Diederik Krols <diederik_krols@hotmail.com>
2023-10-29 19:12:34 +0100

    Functions Page.

---
+++ XamlBrewer.WinUI3.LiveChart2.Sample/ViewModels/FunctionsViewModel.cs
@@ -0,0 +1,61 @@
+﻿using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
+using System;
+using System.Collections.Generic;
+
+namespace XamlBrewer.WinUI3.LiveCharts2.Sample.ViewModels
+{
+    public class FunctionsViewModel
+    {
+        public FunctionsViewModel()
+        {
+            Func<double, double> batFunction1 = (x) => 2 * Math.Sqrt(-Math.Abs(Math.Abs(x) - 1) * Math.Abs(3 - Math.Abs(x)) / ((Math.Abs(x) - 1) * (3 - Math.Abs(x)))) * (1 + Math.Abs(Math.Abs(x) - 3) / (Math.Abs(x) - 3)) * Math.Sqrt(1 - Math.Pow((x / 7), 2)) + (5 + 0.97 * (Math.Abs(x - 0.5) + Math.Abs(x + 0.5)) - 3 * (Math.Abs(x - 0.75) + Math.Abs(x + 0.75))) * (1 + Math.Abs(1 - Math.Abs(x)) / (1 - Math.Abs(x)));
+            Func<double, double> batFunction2 = (x) => -3 * Math.Sqrt(1 - Math.Pow((x / 7), 2)) * Math.Sqrt(Math.Abs(Math.Abs(x) - 4) / (Math.Abs(x) - 4));
+            Func<double, double> batFunction3 = (x) => Math.Abs(x / 2) - 0.0913722 * (Math.Pow(x, 2)) - 3 + Math.Sqrt(1 - Math.Pow((Math.Abs(Math.Abs(x) - 2) - 1), 2));
+            Func<double, double> batFunction4 = (x) => (2.71052 + (1.5 - .5 * Math.Abs(x)) - 1.35526 * Math.Sqrt(4 - Math.Pow((Math.Abs(x) - 1), 2))) * Math.Sqrt(Math.Abs(Math.Abs(x) - 1) / (Math.Abs(x) - 1)) + 0.9;
+
+            Series = new ISeries[]
+            {
+            new LineSeries<ObservablePoint>
+                {
+                    Values =  FunctionValues(batFunction1, -7, +7, .01),
+                    GeometrySize = 0,
+                    Fill = null
+                },
+            new LineSeries<ObservablePoint>
+                {
+                    Values =  FunctionValues(batFunction2, -7, +7, .01),
+                    GeometrySize = 0,
+                    Fill = null
+                },
+            new LineSeries<ObservablePoint>
+                {
+                    Values =  FunctionValues(batFunction3, -7, +7, .01),
+                    GeometrySize = 0,
+                    Fill = null
+                },
+           new LineSeries<ObservablePoint>
+                {
+                    Values =  FunctionValues(batFunction4, -7, +7, .01),
+                    GeometrySize = 0,
+                    Fill = null
+                }
+            };
+        }
+
+        public ISeries[] Series { get; set; }
+
+        private static ObservablePoint[] FunctionValues(Func<double, double> function, double x0, double x1, double dx)
+        {
+            var values = new List<ObservablePoint>();
+            for (double i = x0; i < x1; i += dx)
+            {
+                var y = function(i);
+                values.Add(new ObservablePoint(i, double.IsNaN(y) ? null : y));
+            }
+
+            return values.ToArray();
+        }
+    }
+}
