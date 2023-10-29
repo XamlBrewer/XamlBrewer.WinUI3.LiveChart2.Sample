fc878234d0e2a6e73f2f40cfa38d0a65f3418931
Diederik Krols <diederik_krols@hotmail.com>
2023-10-29 11:10:00 +0100

    Gallery.

---
+++ XamlBrewer.WinUI3.LiveChart2.Sample/Views/Events/AddPointOnClick/View.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace WinUISample.Events.AddPointOnClick
+{
+    public sealed partial class View : UserControl
+    {
+        public View()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
