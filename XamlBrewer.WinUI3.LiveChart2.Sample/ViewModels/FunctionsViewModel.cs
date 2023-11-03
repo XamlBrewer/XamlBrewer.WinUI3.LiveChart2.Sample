afb823800c1094c2a46bb159fd8dd6f7755bc9f9
Diederik Krols <diederik_krols@hotmail.com>
2023-11-03 09:07:17 +0100

    Functions: code cleanup

--- XamlBrewer.WinUI3.LiveChart2.Sample/ViewModels/FunctionsViewModel.cs
+++ XamlBrewer.WinUI3.LiveChart2.Sample/ViewModels/FunctionsViewModel.cs
@@ -12,10 +12,10 @@ namespace XamlBrewer.WinUI3.LiveCharts2.Sample.ViewModels
     {
         public FunctionsViewModel()
         {
-            Func<double, double> batFunction1 = (x) => 2 * Math.Sqrt(-Math.Abs(Math.Abs(x) - 1) * Math.Abs(3 - Math.Abs(x)) / ((Math.Abs(x) - 1) * (3 - Math.Abs(x)))) * (1 + Math.Abs(Math.Abs(x) - 3) / (Math.Abs(x) - 3)) * Math.Sqrt(1 - Math.Pow((x / 7), 2)) + (5 + 0.97 * (Math.Abs(x - 0.5) + Math.Abs(x + 0.5)) - 3 * (Math.Abs(x - 0.75) + Math.Abs(x + 0.75))) * (1 + Math.Abs(1 - Math.Abs(x)) / (1 - Math.Abs(x)));
-            Func<double, double> batFunction2 = (x) => -3 * Math.Sqrt(1 - Math.Pow((x / 7), 2)) * Math.Sqrt(Math.Abs(Math.Abs(x) - 4) / (Math.Abs(x) - 4));
-            Func<double, double> batFunction3 = (x) => Math.Abs(x / 2) - 0.0913722 * (Math.Pow(x, 2)) - 3 + Math.Sqrt(1 - Math.Pow((Math.Abs(Math.Abs(x) - 2) - 1), 2));
-            Func<double, double> batFunction4 = (x) => (2.71052 + (1.5 - .5 * Math.Abs(x)) - 1.35526 * Math.Sqrt(4 - Math.Pow((Math.Abs(x) - 1), 2))) * Math.Sqrt(Math.Abs(Math.Abs(x) - 1) / (Math.Abs(x) - 1)) + 0.9;
+            static double batFunction1(double x) => 2 * Math.Sqrt(-Math.Abs(Math.Abs(x) - 1) * Math.Abs(3 - Math.Abs(x)) / ((Math.Abs(x) - 1) * (3 - Math.Abs(x)))) * (1 + Math.Abs(Math.Abs(x) - 3) / (Math.Abs(x) - 3)) * Math.Sqrt(1 - Math.Pow((x / 7), 2)) + (5 + 0.97 * (Math.Abs(x - 0.5) + Math.Abs(x + 0.5)) - 3 * (Math.Abs(x - 0.75) + Math.Abs(x + 0.75))) * (1 + Math.Abs(1 - Math.Abs(x)) / (1 - Math.Abs(x)));
+            static double batFunction2(double x) => -3 * Math.Sqrt(1 - Math.Pow((x / 7), 2)) * Math.Sqrt(Math.Abs(Math.Abs(x) - 4) / (Math.Abs(x) - 4));
+            static double batFunction3(double x) => Math.Abs(x / 2) - 0.0913722 * (Math.Pow(x, 2)) - 3 + Math.Sqrt(1 - Math.Pow((Math.Abs(Math.Abs(x) - 2) - 1), 2));
+            static double batFunction4(double x) => (2.71052 + (1.5 - .5 * Math.Abs(x)) - 1.35526 * Math.Sqrt(4 - Math.Pow((Math.Abs(x) - 1), 2))) * Math.Sqrt(Math.Abs(Math.Abs(x) - 1) / (Math.Abs(x) - 1)) + 0.9;
 
             Series = new ISeries[]
             {
@@ -50,9 +50,9 @@ namespace XamlBrewer.WinUI3.LiveCharts2.Sample.ViewModels
             };
         }
 
+        public static SolidColorPaint LegendTextPaint => new(SKColors.Gray);
+        
         public ISeries[] Series { get; set; }
-
-        public SolidColorPaint LegendTextPaint => new SolidColorPaint(SKColors.Gray);
 
         private static ObservablePoint[] FunctionValues(Func<double, double> function, double x0, double x1, double dx)
         {
